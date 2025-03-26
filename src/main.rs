6ae652c15fadd84fc84ad7ee5e04d1760982b1a5
KarlOlofA <karl.98oa@gmail.com>
2025-03-26 22:44:42 +0100

    Actually fetching full repos

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use clap::Parser;
-use reqwest::blocking::Client;
+use reqwest::Client;
 use serde::Deserialize;
 use std::fs::{create_dir_all, File};
 use std::io::Write;
 use std::path::{Path, PathBuf};
+use tokio;
+use async_recursion::async_recursion;
+use std::error::Error;
+use std::fs;
+use serde_json::Value;
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, USER_AGENT};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Cli::parse();
     let url = format!(
         "https://api.github.com/repos/{}/{}/contents/",
@@ -13,19 +20,51 @@ fn main() {
     );
 
     let client = Client::new();
-
-    let _x = fetch_git_contents(&client, &url, &args.output);
+    
+    match get_token() {
+        Ok(token) => {
+            match fetch_git_contents(&token, &client, &url, &args.output).await {
+                Ok(user) => println!("{:#?}", user),
+                Err(e) => eprintln!("Error fetching GitHub user: {}", e),
+            }
+        }
+        Err(e) => eprintln!("Error reading token: {}", e),
+    }
 }
 
-fn fetch_git_contents(client: &Client, url: &String, output: &PathBuf) -> reqwest::Result<()> {
+fn get_token() -> Result<String, Box<dyn Error>> {
+    let file_content = fs::read_to_string("config.json")?;
+    
+    let json: Value = serde_json::from_str(&file_content)?;
+    
+    json["token"]
+        .as_str()
+        .map(|s| s.to_string())
+        .ok_or_else(|| "Token not found in config.json".into())
+}
+
+#[async_recursion]
+async fn fetch_git_contents(token: &str, client: &Client, url: &String, output: &PathBuf) -> reqwest::Result<()> {
+
+    let mut headers = HeaderMap::new();
+
+    match HeaderValue::from_str(&format!("token {}", token)) {
+        Ok(val) => {headers.insert(AUTHORIZATION, val);}
+        Err(e) => {
+            println!("{}", e);
+            return Ok(())
+        },
+    }
+    headers.insert(USER_AGENT, HeaderValue::from_static("GitGetter"));
+
     let response = client
         .get(url)
-        .header("User-Agent", "rust-cli")
-        .send()?
-        .json::<Vec<GitHubItem>>()?;
+        .headers(headers)
+        .send().await?;
 
-    // Process each item in the response
-    for item in response {
+    let x = response.json::<Vec<GitHubItem>>().await?;
+
+    for item in x {
         println!("New ITEM -> {:?}", item);
         match item {
             GitHubItem::File {
@@ -34,11 +73,11 @@ fn fetch_git_contents(client: &Client, url: &String, output: &PathBuf) -> reqwes
                 if let Some(url) = download_url {
                     let file_path = output.join(path);
                     println!("Downloading: {}", file_path.display());
-                    download_file(client, &url, &file_path)?;
+                    download_file(client, &url, &file_path).await?;
                 }
             }
             GitHubItem::Directory { path, .. } => {
-                let dir_url = format!("{}/{}", url.trim_end_matches('/'), path);
+                let dir_url = format!("{}/{}", url, path);
                 let dir_path = output.join(path);
                 match create_directory(&dir_path) {
                     Ok(_) => {}
@@ -46,7 +85,7 @@ fn fetch_git_contents(client: &Client, url: &String, output: &PathBuf) -> reqwes
                         println!("Error: {e}");
                     }
                 }
-                fetch_git_contents(client, &dir_url, output)?; // Recursive call for the directory
+                fetch_git_contents(token, client, &dir_url, output).await;
             }
         }
     }
@@ -58,16 +97,16 @@ fn create_directory(path: &Path) -> std::io::Result<()> {
     if !path.exists() {
         create_dir_all(path)
     } else {
-        Ok(())  // The directory already exists, no need to do anything
+        Ok(()) 
     }
 }
 
-fn download_file(client: &Client, url: &str, file_path: &Path) -> reqwest::Result<()> {
-    let response = client.get(url).header("User-Agent", "rust-cli").send()?;
+async fn download_file(client: &Client, url: &str, file_path: &Path) -> reqwest::Result<()> {
+    let response = client.get(url).header("User-Agent", "rust-cli").send().await?;
 
-    match response.bytes() {
+    match response.bytes().await {
         Ok(bytes) => {
-            let mut file = File::create(file_path);
+            let file = File::create(file_path);
             match file {
                 Ok(mut file) => {
                     file.write_all(&bytes);},
